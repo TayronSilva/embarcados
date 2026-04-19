49fca678c76912bc8d6df948aa8e19ecb1747a12
Tayronne Silva <ttayronne.silva@gmail.com>
2026-04-19 14:51:00 -0300

    modifying the driver entity and service class

--- backend/src/main/java/com/embarcados/api/features/driver/domain/DriverEntity.java
+++ backend/src/main/java/com/embarcados/api/features/driver/domain/DriverEntity.java
@@ -20,25 +20,4 @@ public class DriverEntity {
     private String password;
     private String companyId;
 }
-package com.embarcados.api.features.driver.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-@Data
-@Document(collection = "driver")
-@NoArgsConstructor
-@AllArgsConstructor
-public class DriverEntity {
-
-    @Id
-    private String id;
-    private String name;
-    private String email;
-    private String document;
-    private String password;
-
-    private String companyId;
-}
